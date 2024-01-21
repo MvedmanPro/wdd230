64316291c3d8857af733b454216c81a8bfbedfe1
MvedmanPro <mailkgpage@gmail.com>
2024-01-20 23:40:07 -0500

    Lesson02 Webdesign Principles, modified js for lesson01

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,9 @@
 // current year
-var year = new Date().getFullYear();
+const year = new Date().getFullYear();
 
 // last modified date
-var lastModified = document.lastModified;
+const lastModified = document.lastModified;
 
 // Output the year and last modified
-document.getElementById('currentyear').textContent = year + " | Kateryna Glazkova | Cambodia/USA";
-document.getElementById('lastmodified').textContent = "Last Modified: " + lastModified;
+document.getElementById('currentyear').textContent = `${year} | Kateryna Glazkova | Cambodia/USA`;
+document.getElementById('lastmodified').textContent = `Last Modified: ${lastModified}`;
