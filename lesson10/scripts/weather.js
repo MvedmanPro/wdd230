e22c440f60eada90a0e58a8efda036f8ab36ffec
MvedmanPro <mailkgpage@gmail.com>
2024-03-16 04:26:32 -0400

    corrected js w10

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -5,7 +5,7 @@ const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
 
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.66&appid=b409ee506ffe2f7f43e8a343d4018ab0&units=imperial" 
-//const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=b865574f47333eba1210cb2956e0e422&units=imperial"
+
 async function apiFetch() {
     try {
         const response = await fetch(url)
