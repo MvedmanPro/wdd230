7a9eecd44b6bce40b2654dcccbd97af7eee8be04
MvedmanPro <mailkgpage@gmail.com>
2024-03-16 04:20:51 -0400

    w10 completed weather exercise

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -22,3 +22,22 @@ async function apiFetch() {
 }
 
 apiFetch();
+
+function displayResults(data) {
+    const temp = Math.round(data.main.temp)
+    currentTemp.innerHTML = `${temp}&deg;F`
+    const iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+    let desc = data.weather[0].description
+    let capDesc = titleCase(desc)
+
+    weatherIcon.setAttribute("src", iconSrc)
+    weatherIcon.setAttribute("alt", "Weather icon")
+    captionDesc.textContent = capDesc
+
+}
+
+function titleCase(desc) {
+    return desc.replace(/\b\w/g, function (char) {
+        return char.toUpperCase()
+    })
+}
