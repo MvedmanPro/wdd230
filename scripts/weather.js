be00ee9fc1e8f3807322d109a2ec3186de281042
MvedmanPro <mailkgpage@gmail.com>
2024-03-16 05:21:17 -0400

    w10, course home page added weather API

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,8 +1,10 @@
-const currentTemp = document.querySelector("#current-temp");
+const currentTemp = document.querySelector("#temperature");
 const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.66&appid=b409ee506ffe2f7f43e8a343d4018ab0&units=imperial" 
+
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=46.48&lon=30.74&appid=b409ee506ffe2f7f43e8a343d4018ab0&units=metric" 
 
 async function apiFetch() {
     try {
