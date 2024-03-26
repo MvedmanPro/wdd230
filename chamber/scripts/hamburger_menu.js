53adc0587a87545fa85f3ec0974ab5da2e1f734f
MvedmanPro <mailkgpage@gmail.com>
2024-03-25 20:50:55 -0400

    Changed many things in layot to avoid CLS on Home Page

--- chamber/scripts/hamburger_menu.js
+++ chamber/scripts/hamburger_menu.js
@@ -1,7 +1,11 @@
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('#menu');
+const mainnav = document.querySelector('#navigation')
+const hambutton = document.querySelector('#hamButton');
 
 hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('show');
     hambutton.classList.toggle('show');
 });
+const joinButton = document.querySelector("#joinBtn")
+joinButton.addEventListener("click", () => {
+    window.location.href="join.html"
+})
